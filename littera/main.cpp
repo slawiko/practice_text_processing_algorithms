00d2705ddf67734a0397bbc2793659760512a988
slawiko <shchaurouski.slava@gmail.com>
2019-01-09 16:04:47 +0300

    working example

--- littera/main.cpp
+++ littera/main.cpp
@@ -2,11 +2,41 @@
 #include <fstream>
 #include <vector>
 
-bool is_char(char &c) {
+unsigned int min(unsigned int a, unsigned int b) {
+    return a <= b ? a : b;
+}
+
+void z_function(unsigned int* z, std::string &s) {
+    int n = (int) s.length();
+    for (int i = 1, l = 0, r = 0; i < n; ++i) {
+        if (i <= r)
+            z[i] = min(r - i + 1, z[i - l]);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+            ++z[i];
+        if (i + z[i] - 1 > r)
+            l = i, r = i + z[i] - 1;
+    }
+}
+
+void prefix_function(unsigned int* pi, std::string s) {
+    int n = (int) s.length();
+    for (int i = 1; i < n; ++i) {
+        unsigned int j = pi[i - 1];
+        while (j > 0 && s[i] != s[j]) {
+            j = pi[j - 1];
+        }
+        if (s[i] == s[j]) {
+            ++j;
+        }
+        pi[i] = j;
+    }
+}
+
+bool is_char(const char &c) {
     return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
 }
 
-std::string calculate_diff(std::string &s1, std::string &s2, int n) {
+std::string calculate_diff(const std::string &s1, const std::string &s2, int n) {
     char diff = 0;
     int skipped = 0;
     char* result = new char[n];
@@ -21,7 +51,7 @@ std::string calculate_diff(std::string &s1, std::string &s2, int n) {
             diff += 26;
         }
 
-        result[i - skipped] = diff + 'a' - 1;
+        result[result_size] = diff + 'a' - 1;
         result_size++;
     }
 
@@ -62,20 +92,6 @@ void readlines(int n, std::string &s, std::ifstream &in) {
     }
 }
 
-void prefix_function(unsigned int* pi, std::string s) {
-    int n = (int) s.length();
-    for (int i = 1; i < n; ++i) {
-        unsigned int j = pi[i - 1];
-        while (j > 0 && s[i] != s[j]) {
-            j = pi[j - 1];
-        }
-        if (s[i] == s[j]) {
-            ++j;
-        }
-        pi[i] = j;
-    }
-}
-
 void print(unsigned int* v, int n) {
     for (int i = 0; i < n; ++i) {
         std::cout << v[i] << ' ';
@@ -98,6 +114,16 @@ int period_size(unsigned int* pi, int n) {
     return n;
 }
 
+int period_size_2(unsigned int* z, int n) {
+    for (int i = 0; i < n; ++i) {
+        if (i + z[i] == n) {
+            return i;
+        }
+    }
+
+    return n;
+}
+
 int main() {
     std::ifstream in("littera.in");
     std::ofstream out("littera.out");
@@ -114,13 +140,17 @@ int main() {
     std::string diff = calculate_diff(s1, s2, n);
     int diff_size = (int) diff.length();
 
-    unsigned int* pi = new unsigned int[diff_size];
+//    unsigned int* pi = new unsigned int[diff_size];
+    unsigned int* z = new unsigned int[diff_size];
     for (int i = 0; i < diff_size; ++i) {
-        pi[i] = 0;
+//        pi[i] = 0;
+        z[i] = 0;
     }
-    prefix_function(pi, diff);
+//    prefix_function(pi, diff);
+    z_function(z, diff);
 
-    int period = period_size(pi, diff_size);
+//    int period = period_size(pi, diff_size);
+    int period = period_size_2(z, diff_size);
     out << min_cyclic_shift(diff.substr(0, period));
 
     out.close();
